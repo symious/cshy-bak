c80e7ae658f7c687655c416af2a7e639e9568f0d
Symious <yiyang0203@gmail.com>
2022-01-17 18:54:51 +0800

    chapter 7

---
+++ cpp_primer/chapter7/exercise7.35.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+typedef std::string Type;
+Type initVal();
+class Exercise {
+public :
+    typedef double Type;
+    Type setVal(Type);
+    Type initVal();
+private:
+    int val;
+};
+Exercise::Type Exercise::setVal(Type parm) {
+    val = parm + initVal();
+    return val;
+}
