3f3d58b7257f72da6b503ad492d981be3b8ff534
Symious <yiyang0203@gmail.com>
2022-02-11 10:38:56 +0800

    scope

---
+++ cppprimer/chapter6/scope.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string read();
+void print(const string &);
+void print(double);
+void fooBar(int ival) {
+    // bool read = false;
+    string s = read();
+    void print(int);
+    void print(double);
+    print("Value:");
+    print(ival);
+    print(3.14);
+}
+
+int main()
+{
+    fooBar(1);
+}
